57bda25c6654154777cca96ecb46930a59e48618
ChokkaWindows <chokkamedex@aol.com>
2016-12-17 17:54:46 -0500

    Project Prototype
    Project Prototype

---
+++ MyCity/MyCity/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MyCity
+{
+    public class App : Application
+    {
+        public App()
+        {
+
+            MainPage = new MyCity.MainPage();
+        }
+
+       
+    }
+}
